dd3596527d9f4564eca9fcf8ab6de97a79926610
Bucky24 <rwijtman@gmail.com>
2022-09-19 21:08:36 -0700

    basic env handling

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -1,9 +1,36 @@
-from grammar import Tree
+from transformer import TYPES
 
 def unwrap_statement(statement):
-    if 
+    if statement['type'] == TYPES['STATEMENT']:
+        # a statement should only ever have 1 child
+        return statement['statement']
+
+    return statement
 
 # this method handles all the pragmas as well as determining what statements fall under what blocks
 def preprocess(tree):
+    frontend = []
+    backend = []
+
+    active = backend
+
+    def switch_env(env):
+        nonlocal active
+        if env == "frontend":
+            active = frontend
+        elif env == "backend":
+            active = backend
+
     for statement in tree:
-        print(statement)
+        unwrapped_statement = unwrap_statement(statement)
+        if unwrapped_statement['type'] == TYPES['PRAGMA']:
+            pragma = unwrapped_statement['pragma']
+            if pragma == 'frontend' or pragma == "backend":
+                switch_env(pragma)
+        else:
+            active.append(statement)
+
+    return {
+        "frontend": frontend,
+        "backend": backend,
+    }
--- src/preprocessor_test.py
+++ src/preprocessor_test.py
@@ -1,8 +1,19 @@
 import unittest
 
+if 'unittest.util' in __import__('sys').modules:
+    # Show full diff in self.assertEqual.
+    __import__('sys').modules['unittest.util']._MAX_LENGTH = 999999999
+
 from grammar import parse_statement
-from transformer import process_tree
+from transformer import process_tree, TYPES
 from preprocessor import preprocess
+
+def statement(data, spaces):
+    return {
+        "type": TYPES["STATEMENT"],
+        "spaces": spaces,
+        "statement": data,
+    }
 
 class TestPreprocessor(unittest.TestCase):
     def test_frontend_pragma(self):
@@ -10,7 +21,67 @@ class TestPreprocessor(unittest.TestCase):
         processed = process_tree(tree)
         preprocessed = preprocess(processed)
 
-        print(preprocessed)
+        self.assertEqual(preprocessed['backend'], [])
+        self.assertEqual(preprocessed['frontend'],[
+            statement({
+                "type": TYPES['VARIABLE_ASSIGNMENT'],
+                "name": "foo",
+                "value": statement('"abcd"', 0),
+            }, 0)
+        ])
+
+    def test_backend_pragma(self):
+        tree = parse_statement("#backend\nfoo = 'abcd'")
+        processed = process_tree(tree)
+        preprocessed = preprocess(processed)
+
+        self.assertEqual(preprocessed['frontend'], [])
+        self.assertEqual(preprocessed['backend'],[
+            statement({
+                "type": TYPES['VARIABLE_ASSIGNMENT'],
+                "name": "foo",
+                "value": statement('"abcd"', 0),
+            }, 0)
+        ])
+
+    def test_default_env(self):
+        tree = parse_statement("foo = 'abcd'")
+        processed = process_tree(tree)
+        preprocessed = preprocess(processed)
+
+        self.assertEqual(preprocessed['frontend'], [])
+        self.assertEqual(preprocessed['backend'],[
+            statement({
+                "type": TYPES['VARIABLE_ASSIGNMENT'],
+                "name": "foo",
+                "value": statement('"abcd"', 0),
+            }, 0)
+        ])
+
+    def test_mixed_env(self):
+        tree = parse_statement("#frontend\nfoo = 'abcd'\n#backend\nbar = '123'\n#frontend\nbin = foo")
+        processed = process_tree(tree)
+        preprocessed = preprocess(processed)
+
+        self.assertEqual(preprocessed['frontend'], [
+            statement({
+                "type": TYPES['VARIABLE_ASSIGNMENT'],
+                "name": "foo",
+                "value": statement('"abcd"', 0),
+            }, 0),
+            statement({
+                "type": TYPES['VARIABLE_ASSIGNMENT'],
+                "name": "bin",
+                "value": statement('foo', 0),
+            }, 0)
+        ])
+        self.assertEqual(preprocessed['backend'],[
+            statement({
+                "type": TYPES['VARIABLE_ASSIGNMENT'],
+                "name": "bar",
+                "value": statement('"123"', 0),
+            }, 0)
+        ])
 
 if __name__ == "__main__":
     unittest.main()
