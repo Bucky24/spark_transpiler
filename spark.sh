715de92326f2ed425505130a09d1a0867d41234a
Robbert Wijtman <rwijtman@slack-corp.com>
2023-05-11 09:40:28 -0700

    more fix

--- spark.sh
+++ spark.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-if [[ $# -eq 0 ]]; then
-    echo "Usage: spark <file to compile>"
-    exit 1
-fi
-
 # from https://stackoverflow.com/a/1638397/8346513
 # Absolute path to this script, e.g. /home/user/bin/foo.sh
-SCRIPT=$(readlink "$0")
+SCRIPT=$(readlink -f "$0")
 # Absolute path this script is in, thus /home/user/bin
 SCRIPTPATH=$(dirname "$SCRIPT")
 
